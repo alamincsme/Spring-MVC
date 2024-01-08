18ffe441a3aa6113b8ed27294e80645a9293ce7a
alamincsme <alamincsme@gmail.com>
2024-01-08 10:00:28 +0600

    Write custom query in programmer repo

--- src/main/java/com/alamincsme/springboot/Repository/ProgrammerRepo.java
+++ src/main/java/com/alamincsme/springboot/Repository/ProgrammerRepo.java
@@ -1,6 +1,9 @@
 package com.alamincsme.springboot.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.alamincsme.springboot.Programmer;
@@ -8,5 +11,12 @@ import com.alamincsme.springboot.Programmer;
 
 @Repository
 public interface ProgrammerRepo extends JpaRepository<Programmer, Integer> {
+
+
+	
+    @Query("from Programmer where pName = ?1")
+	List<Programmer> findP(String pName);
+
+	
 	
 }
